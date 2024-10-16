e110c0add9bb2622444991ef1891f634d14dbb94
JoaoMarcosVilela <joaosemp12345@gmail.com>
2024-10-16 12:28:26 -0300

    Adicionando a feature de post

--- controllers/filmeController.js
+++ controllers/filmeController.js
@@ -6,6 +6,17 @@ const filmeController = {
         let results = await FilmeModel.find({});
         res.send(results).status(200);
     },
+    create: async(req, res)=>{
+        try {
+            const filme = {
+                nome: req.body.nome
+            };
+            const response = await FilmeModel.create(filme);
+            res.status(201).json({response, msg:"Usuário cadastrado!"});
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }
 
 module.exports = filmeController;
--- routes/filmes.js
+++ routes/filmes.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const filmeController = require("../controllers/filmeController");
 
 router.route("/filmes").get((req, res)=> filmeController.readAll(req,res));
+router.route("/filmes").post((req, res)=> filmeController.create(req,res));
 
 module.exports = router;
